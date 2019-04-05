ca97ce37b08cb0df406a13d9639e230f94938db3
yinuo yang <yinuo.yang@ufl.edu>
2019-04-05 13:48:00 -0400

    run yapf on molssi_math

--- molssi_devops/molssi_math.py
+++ molssi_devops/molssi_math.py
@@ -5,6 +5,7 @@ best practices workshop
 Handles the primary functions
 """
 
+
 def mean(num_list):
     """
 	Calculate the mean/average of a list of numbers.
@@ -20,7 +21,7 @@ def mean(num_list):
         The means of the list
     """
 
-    mean_list = sum(num_list)/len(num_list)
+    mean_list = sum(num_list) / len(num_list)
 
     return mean_list
 
